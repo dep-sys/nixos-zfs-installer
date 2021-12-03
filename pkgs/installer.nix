90c1a68637ffd2b8ac0bedca8f07ab722c14f97e
phaer <hello@phaer.org>
2021-12-04 00:43:32 +0100

    fix broken multiline comment

--- pkgs/installer.nix
+++ pkgs/installer.nix
@@ -30,7 +30,7 @@ let
     jq . runtime-info.json
 
     # We base64 encode the runtime data and pass it via linux kernel parameter
-    to our nixos kexec environment to configure our flake there.
+    # to our nixos kexec environment to configure our flake there.
     RUNTIME_KERNEL_PARAMETER="$(jq -c . runtime-info.json | base64 -w0)"
 
     # Adapted from https://gist.github.com/Mic92/4fdf9a55131a7452f97003f445294f97
