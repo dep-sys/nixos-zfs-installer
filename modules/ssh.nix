a1f46be1a7eba217e56ed88b8ceb107ae57f5d2d
phaer <hello@phaer.org>
2021-12-06 11:48:23 +0100

    fix ssh module, use lib.mkMerge...
    ...because otherwise nix silently drops the first config block

--- modules/ssh.nix
+++ modules/ssh.nix
@@ -4,20 +4,22 @@
   # Over there, we rely on an systemd service to read the key from
   # runtime_info kernel cmdline and write it to tmpfs.
   config =
-    {
-      services.openssh = {
-        enable = true;
-        passwordAuthentication = lib.mkForce false;
-        permitRootLogin = lib.mkForce "without-password";
-      };
-    }
-    // (lib.mkIf (config.runtimeInfo) {
-      users.users.root.openssh.authorizedKeys.keys = config.runtimeInfo.rootAuthorizedKeys;
-    })
-    // (lib.mkIf (!config.runtimeInfo) {
-    services.openssh.extraConfig = ''
+    lib.mkMerge [
+      ({
+        services.openssh = {
+          enable = true;
+          passwordAuthentication = lib.mkForce false;
+          permitRootLogin = lib.mkForce "without-password";
+        };
+      })
+      (lib.mkIf (config.runtimeInfo) {
+        users.users.root.openssh.authorizedKeys.keys = config.runtimeInfo.rootAuthorizedKeys;
+      })
+      (lib.mkIf (!config.runtimeInfo) {
+        services.openssh.extraConfig = ''
             match User root
                 AuthorizedKeysFile /var/run/keys/root-authorized-keys
             '';
-    });
+      })
+    ];
 }
