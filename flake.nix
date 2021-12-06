dc5587d80e117ad5d24396b0ab920ec71b51a90e
phaer <hello@phaer.org>
2021-12-06 12:39:30 +0100

    cleanup, mostly obsolete comments from templates

--- flake.nix
+++ flake.nix
@@ -1,20 +1,16 @@
 {
   description = "An optionated nixos installer";
-
-  # Nixpkgs / NixOS version to use.
   inputs.nixpkgs.url = "nixpkgs/nixos-21.11";
 
   outputs = { self, nixpkgs }@inputs:
     let
       # System types to support.
       system = "x86_64-linux";
-
       # Nixpkgs instantiated for supported system types.
       nixpkgsForSystem = import nixpkgs { inherit system; overlays = [ self.overlay ]; };
     in
     {
 
-      # A Nixpkgs overlay.
       overlay = final: prev: {
         kexec = prev.callPackage ./installer/installer.nix {
           inherit nixpkgs system;
@@ -22,18 +18,12 @@
         };
       };
 
-      # Provide some binary packages for selected system types.
       packages.${system} =
         {
-          inherit (nixpkgsForSystem) hello kexec;
+          inherit (nixpkgsForSystem) kexec;
         };
+     defaultPackage.${system} = self.packages.${system}.kexec;
 
-      # The default package for 'nix build'. This makes sense if the
-      # flake provides only one package or there is a clear "main"
-      # package.
-      defaultPackage.${system} = self.packages.${system}.hello;
-
-      # A NixOS module, if applicable (e.g. if the package provides a system service).
       nixosModules = {
         core = import ./modules/core.nix;
         ssh = import ./modules/ssh.nix;
@@ -126,56 +116,5 @@
             })
           ];
         };
-
-
-      # # Tests run by 'nix flake check' and by Hydra.
-      # checks = forAllSystems
-      #   (system:
-      #     with nixpkgsFor.${system};
-
-      #     {
-      #       inherit (self.packages.${system}) hello;
-
-      #       # Additional tests, if applicable.
-      #       test = stdenv.mkDerivation {
-      #         name = "hello-test-${version}";
-
-      #         buildInputs = [ hello ];
-
-      #         unpackPhase = "true";
-
-      #         buildPhase = ''
-      #           echo 'running some integration tests'
-      #           [[ $(hello) = 'Hello Nixers!' ]]
-      #         '';
-
-      #         installPhase = "mkdir -p $out";
-      #       };
-      #     }
-
-      #     // lib.optionalAttrs stdenv.isLinux {
-      #       # A VM test of the NixOS module.
-      #       vmTest =
-      #         with import (nixpkgs + "/nixos/lib/testing-python.nix") {
-      #           inherit system;
-      #         };
-
-      #         makeTest {
-      #           nodes = {
-      #             client = { ... }: {
-      #               imports = [ self.nixosModules.hello ];
-      #             };
-      #           };
-
-      #           testScript =
-      #             ''
-      #               start_all()
-      #               client.wait_for_unit("multi-user.target")
-      #               client.succeed("hello")
-      #             '';
-      #         };
-      #     }
-      #   );
-
     };
 }
