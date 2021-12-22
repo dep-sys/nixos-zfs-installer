93cf0424080c0ad4745637ded8dee08d16010e71
phaer <hello@phaer.org>
2021-12-22 13:03:20 +0100

    remove installation env from repl

--- repl.nix
+++ repl.nix
@@ -4,16 +4,4 @@ let
   pkgs = import nixpkgs { system = "x86_64-linux"; };
   system = "x86_64-linux";
   lib = flake.inputs.nixpkgs.lib;
-  installationEnvironment = flake.outputs.nixosModules.installationEnvironment;
-  config = (import "${nixpkgs}/nixos" {
-    inherit system;
-    configuration = {
-      imports = [
-        "${nixpkgs}/nixos/modules/installer/netboot/netboot-minimal.nix"
-        installationEnvironment
-      ];
-    };
-  }).config;
-in
-{ inherit flake nixpkgs pkgs lib config;
-}
+in { inherit flake nixpkgs pkgs lib; }
